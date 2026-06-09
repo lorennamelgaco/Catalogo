d6961ae81057cdde239f94a5e25b38ac10395d2a
lorennamelgaco <lorenna.melgaco@uni9.edu.br>
2026-06-09 02:46:46 +0000

    produtos atualizado

--- Catálogo/Controllers/ProdutoController.cs
+++ Catálogo/Controllers/ProdutoController.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Catálogo.Models;
 
@@ -8,8 +7,8 @@ public class ProdutoController : Controller {
 
     public IActionResult Index()
     {
-        var Produto = new List<string> { "LuckStrike", "Malboro", "Gift", "Eight" };
-    var Preco = new List<double> { 6.00, 8.00, 4.00, 6.00 };
+        var Produto = new List<string> { "LuckStrike", "Malboro", "Gift", "Eight", "Camel" };
+    var Preco = new List<double> { 6.00, 8.00, 4.00, 6.00, 12.00 };
 
     ViewBag.Produto = Produto;
     ViewBag.Preco = Preco;
