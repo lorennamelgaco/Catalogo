af5aa3b657e5d6a92219753fdc038b53a6d4f7c6
lorennamelgaco <lorenna.melgaco@uni9.edu.br>
2026-06-09 00:44:14 +0000

    catalago

---
+++ Catálogo/Controllers/ProdutoController.cs
@@ -0,0 +1,19 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Catálogo.Models;
+
+namespace Catálogo.Controllers;
+
+public class ProdutoController : Controller {
+
+    public IActionResult Index()
+    {
+        var Produto = new List<string> { "LuckStrike", "Malboro", "Gift", "Eight" };
+    var Preco = new List<double> { 6.00, 8.00, 4.00, 6.00 };
+
+    ViewBag.Produto = Produto;
+    ViewBag.Preco = Preco;
+
+    return View();
+    }
+}
---
+++ Catálogo/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Catálogo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
